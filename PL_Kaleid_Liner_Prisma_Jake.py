883fae85e8011f85e3f816950eed4a8094e3ca34
raychensg <raychensg@gmail.com>
2014-08-23 01:20:40 -0700

    ThisIsCamelCase

--- PL_Kaleid_Liner_Prisma_Jake.py
+++ PL_Kaleid_Liner_Prisma_Jake.py
@@ -11,18 +11,18 @@ class PrismaJakeMain:
         self.mousex, self.mousey = 0, 0
 
         #color data; not useful
-        self.redColor = pygame.Color(255, 0, 0)
-        self.greenColor = pygame.Color(0, 255, 0)
-        self.blueColor = pygame.Color(0, 0, 255)
-        self.whiteColor = pygame.Color(255, 255, 255)
+        self.red_color = pygame.Color(255, 0, 0)
+        self.green_color = pygame.Color(0, 255, 0)
+        self.blue_color = pygame.Color(0, 0, 255)
+        self.white_color = pygame.Color(255, 255, 255)
         self.black_color = pygame.Color(0, 0, 0)
 
         #self.example_image = pygame.image.load('Assets/cat.png')
 
-        self.fpsClock = pygame.time.Clock()
-        self.fontObj = pygame.font.Font('freesansbold.ttf', 32) #have fang pick a font
+        self.fps_clock = pygame.time.Clock()
+        self.font_obj = pygame.font.Font('freesansbold.ttf', 32) #have fang pick a font
         self.msg = "Begin!"
-        self.soundObj = pygame.mixer.Sound('Assets/oww.wav') #change
+        self.sound_obj = pygame.mixer.Sound('Assets/oww.wav') #change
 
         self.screen = pygame.display.set_mode((self.width, self.height), FULLSCREEN)
     	self.width , self.height = self.screen.get_size()
@@ -30,23 +30,17 @@ class PrismaJakeMain:
 
     def MainLoop(self):
 	    while True:
-	        self.screen.fill(self.whiteColor)
+	        self.screen.fill(self.black_color)
 
-	        pygame.draw.circle(self.screen, self.blueColor, (self.width/2, self.height/2), 20, 0)
-	        pygame.draw.line(self.screen, self.blueColor, (self.width/2, self.height/2), (self.mousex, self.mousey), 4)
-
-	        pixArr = pygame.PixelArray(self.screen)
-	        for x in range(100, 200, 4):
-	        	for y in range(100, 200, 4):
-	        		pixArr[x][y] = self.redColor
-	        del pixArr
+	        pygame.draw.circle(self.screen, self.blue_color, (self.width/2, self.height/2), 20, 0)
+	        pygame.draw.line(self.screen, self.blue_color, (self.width/2, self.height/2), (self.mousex, self.mousey), 4)
 
 	        #self.screen.blit(self.catSurfaceObj, (self.mousex, self.mousey))
 
-	        msgSurfaceObj = self.fontObj.render(self.msg, False, self.blueColor)
-	        msgRectobj = msgSurfaceObj.get_rect()
-	        msgRectobj.topleft = (10, 20)
-	        self.screen.blit(msgSurfaceObj, msgRectobj)
+	        msg_surface_obj = self.font_obj.render(self.msg, False, self.blueColor)
+	        msg_rect_obj = msg_surface_obj.get_rect()
+	        msg_rect_obj.topleft = (10, 20)
+	        self.screen.blit(msg_surface_obj, msg_rect_obj)
 
 	        for event in pygame.event.get():
 	        	if event.type == QUIT:
@@ -56,7 +50,7 @@ class PrismaJakeMain:
 	        		self.mousex, self.mousey = event.pos
 	        	elif event.type == MOUSEBUTTONUP:
 	        		self.mousex, self.mousey = event.pos
-	        		self.soundObj.play()
+	        		self.sound_obj.play()
 	        		if event.button in (1, 2, 3):
 	        			self.msg = 'left, middle, or right mouse click'
 	        		elif event.button in (4, 5):
@@ -71,7 +65,7 @@ class PrismaJakeMain:
 	        			pygame.event.post(pygame.event.Event(QUIT))
 
 		pygame.display.update()
-		self.fpsClock.tick(30)
+		self.fps_clock.tick(30)
 
 if __name__ == "__main__":
  	MainWindow = PrismaJakeMain()
